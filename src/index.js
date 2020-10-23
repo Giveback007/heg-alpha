38b666aabb2096f47c244f8e57fa202fc53e27e5
giveback007 <dovy91@gmail.com>
2020-10-23 15:17:04 -0400

    ble working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,63 @@
-import { main } from './main';
-import * as serviceWorker from './service-worker';
+import { main } from './main'
 
 main();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-serviceWorker.register();
+
+if (process.env.NODE_ENV !== 'development') {
+    console.log('dev')
+}
+
+if (process.env.NODE_ENV === 'production') {
+    // import * as serviceWorker from './service-worker';
+    const serviceWorker = require('./service-worker');
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    serviceWorker.register();
+}
+
+const elm = (id) => document.getElementById(id);
+
+function updateDeviceStatus(device) {
+    setInterval(() => {
+        elm("device").innerHTML = device.name;
+        elm("device-id").innerHTML = device.id;
+        elm("device-connected").innerHTML = device.gatt.connected;
+    }, 1000)
+    
+}
+
+document.getElementById("bt").onclick = () => {
+
+    const serviceUUID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
+    const rxUUID='6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+    const txUUID='6e400003-b5a3-f393-e0a9-e50e24dcca9e';
+    const encoder = new TextEncoder("utf-8");
+    const decoder = new TextDecoder("utf-8");
+
+    navigator.bluetooth.requestDevice({
+        acceptAllDevices: true,
+        optionalServices: [serviceUUID] //
+    }).then(device => {
+        updateDeviceStatus(device);        
+        return device.gatt.connect();
+    })
+    .then(server => server.getPrimaryService(serviceUUID))
+    .then(service => { 
+        service.getCharacteristic(rxUUID).then(tx => {
+            return tx.writeValue(encoder.encode("t"));
+        });
+        return service.getCharacteristic(txUUID)
+    })
+    .then(characteristic=>{
+        return characteristic.startNotifications();
+    })
+    .then(characteristic => {
+        characteristic.addEventListener('characteristicvaluechanged', blenotification)
+    })
+    .catch(err => console.error(err));
+    
+    function blenotification(e){
+        const val = decoder.decode(e.target.value);
+        document.getElementById("output").innerHTML = val;
+    }
+}
